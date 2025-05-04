75f63a15c126622e178da98cf3237b3592b8a417
yogesh1810web <yogi20041018@gmail.com>
2025-05-04 18:22:16 +0530

    Update and rename sample. html.txt to sample.java

---
+++ sample.java
@@ -0,0 +1,18 @@
+public class Pyramid {
+    public static void main(String[] args) {
+        int rows = 5; // You can change this value for a bigger or smaller pyramid
+
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
